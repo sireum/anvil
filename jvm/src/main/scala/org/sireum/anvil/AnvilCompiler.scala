3e73af0f02fc77bd2124ead05743c497e3599eba
Matthew Weis <matthewweis@ksu.edu>
2022-07-19 05:21:49 -0500

    implement hls template

--- jvm/src/main/scala/org/sireum/anvil/AnvilCompiler.scala
+++ jvm/src/main/scala/org/sireum/anvil/AnvilCompiler.scala
@@ -244,7 +244,62 @@ object AnvilCompiler {
     def writeHlsTemplate(): Unit = {
 
       def createHlsTemplate(): String = {
-        return string""
+        // workspace.transpiled holds the hardware-accelerated c code, and we want to ref that in HLS project
+        // added cflags and fno-builtin, see https://www.xilinx.com/html_docs/xilinx2017_4/sdaccel_doc/wqn1504034365505.html
+
+        // lookup list of files contained in "anvil_cfiles.txt" (which was generated during HLS step 1)
+        // each listed file is relative to workspace.transpiled
+        // we want to access them in hls. Hls is launched from project which has in scope our sw/
+        // So really we link transpiled and hls, but are one scope up
+
+        // empty corresponds with return
+        // todo derive filename from config
+        val srcs: ISZ[String] = (workspace.transpiled / string"anvil_cfiles.txt").readLines
+          .map((st: String) => workspace.project.relativize(workspace.transpiled / st).string)
+          .map((s: String) => StringOps(s).replaceAllLiterally("\\", "/"))
+
+        // todo derive filename from config
+        val dirs: ISZ[String] = (workspace.transpiled / string"anvil_include_dirs.txt").readLines
+          .map((st: String) => workspace.project.relativize(workspace.transpiled / st).string)
+          .map((s: String) => StringOps(s).replaceAllLiterally("\\", "/"))
+
+        // todo derive filename from config
+        // the top function's corresponding name in the transpiled c code.
+        {
+          val expected: String = context.template_project_top_function
+          val actual: String = ISZOps((workspace.transpiled / string"anvil_top_function.txt").readLines).first
+          // sanity check: the first line of the file written during transpiler-pass-1 matches the target method
+          expect(expected == actual,
+            st"expected top function $expected did not match the transpiled project's actual top function $actual")
+        }
+
+        // todo derive filename from config
+        // drop the method name (first line) and all other params are "in" ports because return is our "out" port
+        val inPorts: ISZ[String] = ISZOps((workspace.transpiled / string"anvil_top_function.txt").readLines).drop(z"1")
+
+        // empty string represents the function's return value (our "out" port)
+        val ports: ISZ[String] = ISZ(string"") ++ inPorts
+
+        // TODO NOTE: data_pack inPort optimization directives only apply to arrays or array-containing structs
+        // todo how to make this context pluggable across versions? Potentially needs template.
+        // todo re-added -fno-builtin flag to add_files (for manual testing, will remove)
+        // todo re-added -reset flag to open_project (for testing, may remove)
+        return st"""
+                   |open_project -reset ${context.projectWorkspace.hls.name}
+                   |set_top ${context.template_project_top_function}
+                   |${st"${(for (s <- srcs) yield st"add_files $s -cflags ${"\""}-std=c99 -fno-builtin ${(for (d <- dirs) yield st"-I$d -L$d", " ")}${"\""}", "\n")}".render}
+                   |open_solution "${context.template_project_hls_solution}"
+                   |set_part {${hc.template_project_part_number}}
+                   |create_clock -period 10 -name default
+                   |config_export -format ip_catalog -rtl verilog
+                   |config_interface -m_axi_addr64
+                   |set_directive_inline -region -recursive ${"\""}${context.template_project_top_function}${"\""}
+                   |${st"${(for (p <- ports) yield st"set_directive_interface -mode s_axilite ${"\""}${context.template_project_top_function}${"\""} $p", "\n")}".render}
+                   |${st"${(for (in <- inPorts) yield st"set_directive_data_pack -byte_pad field_level ${"\""}${context.template_project_top_function}${"\""} $in", "\n")}".render}
+                   |csynth_design
+                   |export_design -rtl verilog -format ip_catalog
+                   |exit
+                   |""".render
       }
 
       @pure def createHlsShellScript(): String = {
