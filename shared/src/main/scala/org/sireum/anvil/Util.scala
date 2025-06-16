844aafe10a3034db44f955f9ec0bef70048c24d3
Robby <robby@santoslab.org>
2025-06-16 18:59:29 -0500

    Updated starting state label to 4 (from 3).

--- shared/src/main/scala/org/sireum/anvil/Util.scala
+++ shared/src/main/scala/org/sireum/anvil/Util.scala
@@ -1096,7 +1096,8 @@ object Util {
   val kind: String = "Anvil"
   val exitLabel: Z = 0
   val errorLabel: Z = 1
-  val startingLabel: Z = 3
+  val deadLabel: Z = 2
+  val startingLabel: Z = 4
   val returnLocalId: String = "$ret"
   val resultLocalId: String = "$res"
   val constructLocalId: String = "$new"
