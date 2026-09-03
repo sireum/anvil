92aa7fb164e7f2a0a82d9e311d9428ae6dba7ef4
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-09-03 12:24:35 -0500

    Regen Anvil and Ignore Temporal Operators
    Squashed from sireum/anvil#15 (aaurandt:master, 3 commits, head f63c5c7).
    
    Squashed commits:
    * 1c411c9 Regen anvil and mark Unary Temporal Operators as infeasible
    * 114cb52 Regen anvil and mark Binary Temporal Operators as infeasible
    * f63c5c7 Merge remote-tracking branch 'upstream/master'

--- shared/src/main/scala/org/sireum/anvil/IRSimulator.scala
+++ shared/src/main/scala/org/sireum/anvil/IRSimulator.scala
@@ -1276,6 +1276,7 @@ import IRSimulator._
           case AST.IR.Exp.Binary.Op.AppendAll => halt(s"Infeasible: $exp")
           case AST.IR.Exp.Binary.Op.Prepend => halt(s"Infeasible: $exp")
         }
+      case exp: AST.IR.Exp.BinaryTemporal => halt(s"Infeasible: $exp")
       case exp: AST.IR.Exp.Unary =>
         val (v, acs) = evalExp(state, exp.exp)
         exp.op match {
@@ -1284,6 +1285,7 @@ import IRSimulator._
           case AST.Exp.UnaryOp.Plus => return (v, acs)
           case AST.Exp.UnaryOp.Minus => return (v.minus, acs)
         }
+      case exp: AST.IR.Exp.UnaryTemporal => halt(s"Infeasible: $exp")
       case exp: AST.IR.Exp.Type =>
         val (v, acs) = evalExp(state, exp.exp)
         val n: U64 =
